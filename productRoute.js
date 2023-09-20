f0a885ae36cfbba5e4efc608843944554c3cf4b6
sourabhshukla <sourabhshukla9893502147@gmail.com>
2023-09-20 17:08:47 +0530

    Added endpint for gatting data

--- productRoute.js
+++ productRoute.js
@@ -2,12 +2,28 @@ const express = require("express");
 const router = express.Router();
 const Job = require("./models/job.model");
 
+router.route("/").get(async (req, res) => {
+  try {
+    const allJobs = await Job.find({});
+    res.status(200).json({
+      success: true,
+      data: allJobs,
+    });
+  } catch (e) {
+    console.log(e);
+    res.json({
+      sucess: false,
+      msg: "Error",
+    });
+  }
+});
+
 router.route("/").post(async (req, res, next) => {
   let isPresent = await Job.find({ jobId: req.body.jobId });
   console.log(isPresent);
   if (isPresent.length > 0) {
     res.json({
-      success: "false",
+      success: false,
     });
     return;
   }
